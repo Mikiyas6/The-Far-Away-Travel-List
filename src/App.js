a855b8f763a8ca23c5adb1a3d701d502a90a0c15
Mikiyas6 <Mikitedi9@gmail.com>
2024-09-25 22:56:42 +0300

    I implemented controlled elements inside of the form. I let the input elements be controlled by a state variable.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: true },
   { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -19,22 +21,35 @@ function Logo() {
   return <h1>🌴 Far Away 💼</h1>;
 }
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(event);
+    if (!description) return;
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    setDescription("");
+    setQuantity(1);
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => {
+          setQuantity(+e.target.value);
+        }}
+      >
         {Array.from({ length: 20 }, (_, index) => index + 1).map((num) => (
-          <option key={num} value={num}>
-            {num}
-          </option>
+          <option key={num}>{num}</option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        onChange={(e) => setDescription(e.target.value)}
+        type="text"
+        value={description}
+        placeholder="Item..."
+      />
       <button type="text">Add</button>
     </form>
   );
