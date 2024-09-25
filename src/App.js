137ff97938cce94ae00f52c4219f507f66dbbfd7
Mikiyas6 <Mikitedi9@gmail.com>
2024-09-25 19:34:51 +0300

    I implemented the form element

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: true },
   { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 2, description: "Charger", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 12, packed: false },
 ];
 
 export default function App() {
@@ -19,10 +19,24 @@ function Logo() {
   return <h1>🌴 Far Away 💼</h1>;
 }
 function Form() {
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(event);
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-    </div>
+      <select>
+        {Array.from({ length: 20 }, (_, index) => index + 1).map((num) => (
+          <option key={num} value={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button type="text">Add</button>
+    </form>
   );
 }
 function PackingList() {
@@ -30,15 +44,15 @@ function PackingList() {
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item key={item.id} item={item} />
         ))}
       </ul>
     </div>
   );
 }
-function Item({ item }) {
+function Item({ item, key }) {
   return (
-    <li>
+    <li key={key}>
       <span style={item.packed ? { textDecoration: "Line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
